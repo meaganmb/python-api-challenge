817f1c502f25b1fb7fc1edb5534b2c0d6187d6f8
Meagan Murray-Bruce <meaganmb_11@Meagans-MacBook-Pro.local>
2021-07-26 18:48:15 -0700

    modified homework

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "2caca4fa980deb936edb8fd5264f8123"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyAt_EOD_wTikFHLRpiIbXXHi6TAUCduZAg"
